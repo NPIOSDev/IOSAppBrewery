d8f0282d390030c85521a6c8ce075a417c6bc391
Nelson <tomneo2004@gmail.com>
2024-04-03 10:20:35 +0800

    create I am rich project

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Nelson on 3/4/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
