be2c7a5695f13ceb7a291f850945dc3bdb724cfb
Nelson <tomneo2004@gmail.com>
2024-07-25 14:33:42 +0800

    music app

--- Playground/Test.playground/Contents.swift
+++ Playground/Test.playground/Contents.swift
@@ -18,3 +18,37 @@ for i in Range(1...4){
 
 var arr = [12,3,4,5,56]
 print(arr.count)
+
+
+enum MyError: Error {
+    case Unknow
+    case Error1
+    case Error2(code:Int)
+}
+func throwError() throws -> Int {
+    throw MyError.Error1
+}
+
+func throwError2() throws -> Int {
+    throw MyError.Error2(code: 112)
+}
+
+do{
+    try throwError()
+} catch{
+    print("\(error)")
+}
+
+var x = try? throwError()
+print(x as Any)
+
+var x1 = try? throwError2()
+print(x1 as Any)
+
+do {
+    try throwError2()
+} catch {
+    print("\(error)")
+}
+
+
